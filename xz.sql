b7fe202033daf55d2172138c15ccd0ca581af03d
niuxiaona <49236442+niuxiaona@users.noreply.github.com>
2019-04-03 19:45:25 +0800

    Add files via upload

---
+++ xz.sql
@@ -0,0 +1,16 @@
+SET NAMES UTF8;
+DROP DATABASE IF EXISTS xz;
+CREATE DATABASE xz CHARSET=UTF8;
+USE xz;
+/**用户信息**/
+CREATE TABLE xz_user(
+  uid INT PRIMARY KEY AUTO_INCREMENT,
+  uname VARCHAR(32),
+  upwd VARCHAR(32),
+  email VARCHAR(64),
+  phone VARCHAR(16),
+
+  avatar VARCHAR(128),        #头像图片路径
+  user_name VARCHAR(32),      #用户名，如王小明
+  gender INT                  #性别  0-女  1-男
+);
